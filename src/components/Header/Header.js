5b362f57a6bbd5aece56718616f17549c2725ff6
sajib <sajib@appscode.com>
2018-02-07 16:40:45 +0600

    Add React Component

---
+++ src/components/Header/Header.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+// import './App.css';
+
+class Header extends Component {
+  render() {
+    return (
+      <div className="App">
+        My HEADER
+      </div>
+    );
+  }
+}
+
+export default Header;
