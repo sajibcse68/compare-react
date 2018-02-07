869a6a3a03927a9cad15771393e04f0540c044e5
sajib <sajib@appscode.com>
2018-02-07 20:14:03 +0600

    Add generic Router & Footer for every page

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
+import './App.css';
 import React, { Component } from 'react';
 import Header from './components/Header/Header'
+import Home from './components/Home/Home'
 import Footer from './components/Footer/Footer'
-// import './App.css';
+import Faq from './components/Faq/Faq'
+import { Route } from 'react-router-dom'
 
 class App extends Component {
   render() {
+    
     return (
       <div className="App">
 
       <Header />
+
+      <Route exact={true} path="/" component={ Home } />
+      <Route path="/faq" component={ Faq } />
 
       <Footer />
 
